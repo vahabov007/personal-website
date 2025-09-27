91e34f4199217ca2540f1f52dd1e60d82505c849
vahabov007 <vahabvahabov230@gmail.com>
2025-09-27 19:46:53 +0400

    feat: Add Topic field to Get in Touch form, improve security and error handling

--- src/main/java/com/vahabvahabov/personal_website/service/EmailService.java
+++ src/main/java/com/vahabvahabov/personal_website/service/EmailService.java
@@ -1,5 +1,7 @@
 package com.vahabvahabov.personal_website.service;
 
+import jakarta.mail.*;
+
 import jakarta.mail.MessagingException;
 import jakarta.mail.internet.MimeMessage;
 import org.springframework.beans.factory.annotation.Autowired;
