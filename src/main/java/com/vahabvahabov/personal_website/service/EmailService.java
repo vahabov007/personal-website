0b01c34b9186f9b818dbf96aafa8dd38caa69bd0
Vahab Vahabov <vahab.vahabov07@gmail.com>
2025-12-27 12:16:59 +0400

    Enabling Async Functions in EmailService

--- src/main/java/com/vahabvahabov/personal_website/service/EmailService.java
+++ src/main/java/com/vahabvahabov/personal_website/service/EmailService.java
@@ -11,6 +11,7 @@ import org.springframework.core.io.Resource;
 import org.springframework.mail.MailException;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -31,6 +32,7 @@ public class EmailService {
     @Value("${spring.mail.username}")
     private String myEmail;
 
+    @Async
     public String buildHtmlEmailBody(String name, String email, String topic, String message) throws IOException {
         logger.info("Building HTML email body for contact form with topic: {}", topic);
 
